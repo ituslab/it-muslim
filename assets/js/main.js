3f12d0decdbd5d14c4a6ab6d816b674df70de537
Sutan Nst <sutan.gnst@gmail.com>
2018-12-10 16:54:35 +0700

    add detail surah

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,54 +1,13 @@
 // Quran audio cdn files
 // http://cdn.alquran.cloud/media/audio/ayah/ar.alafasy/262
 
-var cdnAudioUrl = function(ayahNumber) {
-    return 'http://cdn.alquran.cloud/media/audio/ayah/ar.alafasy/'+ayahNumber;
+function playAudio(numberAyah) {
+  var
+    audioCdn = 'http://cdn.alquran.cloud/media/audio/ayah/ar.alafasy/'+numberAyah;
+
+  $('#'+numberAyah).html(`
+    <audio id="{{ayah.number}}" autoplay class="ayah-player" controls>
+      <source src="${audioCdn}" type="audio/mpeg">
+    </audio>
+  `)
 }
-
-var currentParent = null;
-
-function onAudioClick(){
-    if(currentParent && 
-            currentParent.data('number') !== 
-            $(this).closest('.control-container').data('number') 
-        ) {
-        var btnAudioEl = document.createElement('button');
-        btnAudioEl.onclick = onAudioClick;
-        btnAudioEl.innerHTML = 'Play audio';
-
-        currentParent.html(btnAudioEl);
-    }   
-
-    var parent = $(this).closest('.control-container');
-    currentParent = parent;
-
-    var dataNumber = parent.data('number');
-
-    var newAudioEl = document.createElement('audio');
-    var newSourceEl = document.createElement('source');
-
-    newSourceEl.src = cdnAudioUrl(dataNumber);
-    newAudioEl.controls = true;
-    newAudioEl.append(newSourceEl);
-
-    var btnClose = document.createElement('button');
-    btnClose.innerHTML = 'Close';
-    btnClose.onclick = onCloseAudioPanel;
-
-    parent.empty();
-
-    parent.append(newAudioEl);
-    parent.append(btnClose);
-}
-
-function onCloseAudioPanel(){
-    var parent = $(this).closest('.control-container');
-
-    var btnAudioEl = document.createElement('button');
-    btnAudioEl.onclick = onAudioClick;
-    btnAudioEl.innerHTML = 'Play audio';
-
-    parent.html(btnAudioEl);
-}
-
-$('.btn-audio').click(onAudioClick);
--- server.js
+++ server.js
@@ -2,61 +2,54 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const nunjucks = require('nunjucks')
 const axios = require('axios').default
-
-const envNunjucks = 
-    new nunjucks.Environment(
-        new nunjucks.FileSystemLoader('templates')
-    )
-
-
+const envNunjucks = new nunjucks.Environment(new nunjucks.FileSystemLoader('templates'))
 
 const app = express()
 envNunjucks.express(app)
 
-
 app.use('/assets',
-    express.static(__dirname + '/assets'),
-    express.static(__dirname + '/node_modules/materialize-css/dist'),
+  express.static(__dirname + '/assets'),
+  express.static(__dirname + '/node_modules/materialize-css/dist'),
 )
+
 app.use('/assets/js',
-    express.static(__dirname + '/node_modules/jquery/dist')
+  express.static(__dirname + '/node_modules/jquery/dist')
 )
+
 app.use(bodyParser.urlencoded({
-    extended:true
+  extended:true
 }))
 
-
-
-app.get('/surah/:surah',(req,res)=>{
-    axios
-        .get(`http://api.alquran.cloud/surah/${req.params.surah}/ar.alafasy`)
-        .then(r=>{
-            const {data} = r.data
-
-            res.render('index.html',{
-                pageTitle:'Index Page',
-                data
-            })
-        })
-        .catch(err=>{
-            console.error('ERR_RESPONSE',err)
-            res.send('ERR')
-        })
+app.get('/surah/:surah', (req, res) => {
+  axios
+  .get(`http://api.alquran.cloud/surah/${req.params.surah}/ar.alafasy`)
+  .then(response => {
+    const {data} = response.data
+    const surah = data
+    res.render('index.html', {
+      pageTitle: surah.englishName,
+      surah
+    })
+  })
+  .catch(err => {
+    console.error('ERR_RESPONSE', err)
+    res.send('ERR')
+  })
 })
 
-app.get('/',(req,res)=>{ 
-    axios.get('http://api.alquran.cloud/surah')
-        .then(r=>{
-            const {data} = r.data
-            res.render('index.html',{
-                pageTitle:'Index Page',
-                data
-            })
-        })
-        .catch(err=>{
-            console.error('ERR_RESPONSE',err)
-            re.send('ERR')
-        })
+app.get('/', (req, res) => {
+  axios.get('http://api.alquran.cloud/surah')
+  .then(r => {
+    const {data} = r.data
+    res.render('index.html', {
+      pageTitle:'Index Page',
+      data
+    })
+  })
+  .catch(err => {
+    console.error('ERR_RESPONSE',err)
+    re.send('ERR')
+  })
 })
 
-app.listen(9696,()=> console.log('server running...'))
+app.listen(9000, () => console.log('server running on http://localhost:9000'))
