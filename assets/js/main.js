ab461568b6cf2cb48680a5540440e7033a0511ca
ariefkahfi <ariefkahfi@gmal.com>
2018-12-11 09:54:27 +0700

    play audio features

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,42 +2,15 @@
 // http://cdn.alquran.cloud/media/audio/ayah/ar.alafasy/262
 
 
-var audioCdn = function(numberAyah) {
-  return 'http://cdn.alquran.cloud/media/audio/ayah/ar.alafasy/'+numberAyah;
-}
-
-var currentParent = null;
-
-function closeAudio(ev){
-  var parent = $(ev).closest('.surah-detail');
-  parent.html(`
-      <a class="waves-effect waves-teal btn-flat btn-audio" onclick="playAudio(this)">Play Audio</a>
-  `);
-}
-
 function playAudio(e){
-  if(currentParent &&
-      currentParent.data('ayah') !== $(e).closest('.surah-detail').data('ayah')
-    ) {
-    console.log('on if...');
-
-    $(currentParent).html(`
-        <a class="waves-effect waves-teal btn-flat btn-audio" onclick="playAudio(this)">Play Audio</a>  
-    `);
-  }
-
   var parent = $(e).closest('.surah-detail');
-  currentParent = parent;
   var ayahNumber = parent.data('ayah');
-  
+  var cdnAudio = 'http://cdn.alquran.cloud/media/audio/ayah/ar.alafasy/'+ayahNumber;
 
   parent.html(`
-      <div class="audio-container">
-        <audio class="audio-player" controls>
-          <source src="${audioCdn(ayahNumber)}"></source>
-        </audio>
-        <a class="waves-effect waves-teal btn-flat btn-close" onclick="closeAudio(this)">Close</a>
-      </div>
+      <audio class="audio-player" controls>
+        <source src="${cdnAudio}"></source>
+      </audio>
   `);  
   
 }
