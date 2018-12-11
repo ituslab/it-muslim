71fb91f052a28d7cd1aa72a9fd1819f988edfa73
ariefkahfi <ariefkahfi@gmal.com>
2018-12-11 10:39:51 +0700

    setup babel

---
+++ src/main.js
@@ -0,0 +1,15 @@
+// Quran audio cdn files
+// http://cdn.alquran.cloud/media/audio/ayah/ar.alafasy/262
+
+
+
+function playAudio(numberAyah) {
+    var
+      audioCdn = 'http://cdn.alquran.cloud/media/audio/ayah/ar.alafasy/'+numberAyah;
+  
+    $('#'+numberAyah).html(`
+      <audio id="{{ayah.number}}" autoplay class="ayah-player" controls>
+        <source src="${audioCdn}" type="audio/mpeg">
+      </audio>
+    `)
+  }
