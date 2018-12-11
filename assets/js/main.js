f5de28ae2e74650f3100c481a263ffbe55644438
Sutan Nst <sutan.gnst@gmail.com>
2018-12-11 10:33:44 +0700

    added babel

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,13 @@
+'use strict';
+
 // Quran audio cdn files
 // http://cdn.alquran.cloud/media/audio/ayah/ar.alafasy/262
 
 
-function playAudio(e){
+function playAudio(e) {
   var parent = $(e).closest('.surah-detail');
   var ayahNumber = parent.data('ayah');
-  var cdnAudio = 'http://cdn.alquran.cloud/media/audio/ayah/ar.alafasy/'+ayahNumber;
+  var cdnAudio = 'http://cdn.alquran.cloud/media/audio/ayah/ar.alafasy/' + ayahNumber;
 
-  parent.html(`
-      <audio class="audio-player" controls>
-        <source src="${cdnAudio}"></source>
-      </audio>
-  `);  
-  
+  parent.html('\n      <audio class="audio-player" controls>\n        <source src="' + cdnAudio + '"></source>\n      </audio>\n  ');
 }
-
