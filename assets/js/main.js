76818d06814973cdef2b63cc5e7e913f819d355f
Sutan Nst <sutan.gnst@gmail.com>
2018-12-11 17:30:57 +0700

    added modal map

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,16 @@
 "use strict";
 
-// Quran audio cdn files
-// http://cdn.alquran.cloud/media/audio/ayah/ar.alafasy/262
-function playAudio(e) {
-  var parent = $(e).closest('.surah-detail');
-  var ayahNumber = parent.data('ayah');
-  var cdnAudio = 'http://cdn.alquran.cloud/media/audio/ayah/ar.alafasy/' + ayahNumber;
-  parent.html("\n      <audio class=\"audio-player\" controls>\n        <source src=\"".concat(cdnAudio, "\"></source>\n      </audio>\n  "));
-}
-
 $(document).ready(function () {
-  var pathName = window.location.pathname.split('/');
-  var surahNumber = pathName[pathName.length - 1];
-  $.ajax({
-    method: 'GET',
-    url: "https://api.alquran.cloud/surah/".concat(surahNumber, "/id.indonesian"),
-    dataType: 'JSON'
-  }).done(function (res) {
-    var data = res.data;
-    var ayahs = data.ayahs;
-    ayahs.forEach(function (_ref) {
-      var numberInSurah = _ref.numberInSurah,
-          text = _ref.text;
-      $("#translate-".concat(numberInSurah)).html("<p>".concat(text, "</p"));
-    });
-  });
+  $('.modal').modal();
 });
+
+var initMap = function initMap() {
+  var uluru = {
+    lat: -25.344,
+    lng: 131.036
+  };
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 8,
+    center: uluru
+  });
+};
