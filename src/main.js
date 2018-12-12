85c521d07ca2a6c2bd80192bd7afca591ca8985c
ariefkahfi <ariefkahfi@gmal.com>
2018-12-12 14:23:39 +0700

    add navbar js code

--- src/main.js
+++ src/main.js
@@ -1,11 +1,25 @@
-$(document).ready(() => {
-  $('.modal').modal()
+$('#nav-toggler').click(()=>{
+  $('#navbar-mobile').css('left','0')
 })
 
-const initMap = () => {
-  uluru = {lat: -25.344, lng: 131.036}
-  map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 8,
-    center: uluru
-  });
+$('#navbar-mobile-close').click(()=>{
+  $('#navbar-mobile').css('left','-100%')
+})
+
+$(window).resize(_=>{
+  if($(window).width() > 992) {
+      $('#navbar-mobile').css('left','-100%')
+  }
+})
+
+$(document).ready(() => {
+$('.modal').modal()
+})
+
+function initMap(){
+let uluru = {lat: -25.344, lng: 131.036};
+let map = new google.maps.Map(document.getElementById('map'), {
+  zoom: 8,
+  center: uluru
+});
 }
