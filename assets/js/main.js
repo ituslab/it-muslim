404c9f84fa28377b8318887e0dd70388b13f77af
ariefkahfi <ariefkahfi@gmal.com>
2018-12-12 14:26:27 +0700

    add navbar code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,27 @@
 "use strict";
 
+$('#nav-toggler').click(function () {
+  $('#navbar-mobile').css('left', '0');
+});
+$('#navbar-mobile-close').click(function () {
+  $('#navbar-mobile').css('left', '-100%');
+});
+$(window).resize(function (_) {
+  if ($(window).width() > 992) {
+    $('#navbar-mobile').css('left', '-100%');
+  }
+});
 $(document).ready(function () {
   $('.modal').modal();
 });
 
-var initMap = function initMap() {
-  uluru = {
+function initMap() {
+  var uluru = {
     lat: -25.344,
     lng: 131.036
   };
-  map = new google.maps.Map(document.getElementById('map'), {
+  var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 8,
     center: uluru
   });
-};
+}
